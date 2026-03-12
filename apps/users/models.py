42fd02e6aeae0bfee4ffee91b61910efc48c33e3
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-12 16:34:00 +0700

    implement create user method in Usermanager

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,2 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Require email')
+        if not username:
+            raise ValueError('Require username')
+        email = self.normalize_email(email)
+        user = self.model(email=email,username=username, **extra_fields)
+        user.set_password(password) #hash password
+        user.save(using=self._db)
+        return user
