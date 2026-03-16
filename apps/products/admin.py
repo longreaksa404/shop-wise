e2670144e7099cc375b0b7815305b4347028f3f8
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 11:43:20 +0700

    implement category admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent_category', 'created_at')
+    list_filter = ('parent_category',)
+    search_fields = ('name',)
+    ordering = ('name',)
