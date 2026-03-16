58f6db67426b83c0e6c92db731f39f7bedc74b3d
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 14:40:38 +0700

    implement cart item admin

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.orders.models import Order, CartItem, OrderItem
 
-# Register your models here.
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('buyer', 'product', 'quantity', 'created_at')
+    search_fields = ('buyer__username',)
+    list_filter = ('created_at',)
+
