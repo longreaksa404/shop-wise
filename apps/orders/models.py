13e7fbac322b8f82a2f4e8093a5b54677d1a601a
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 13:11:51 +0700

    implement order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.conf import settings
 from django.db import models
+from django.db.models import DecimalField
+
 from apps.products.models import Product, Category
 
 class CartItem(models.Model):
@@ -9,5 +11,20 @@ class CartItem(models.Model):
     buyer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='cart_items', on_delete=models.CASCADE, limit_choices_to={'role': 'buyer'})
     product = models. ForeignKey(Product, related_name='cart_items', on_delete=models.SET_NULL)
     quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending',
+        PAID = 'paid', 'Paid',
+        SHIPPED = 'shipped', 'Shipped',
+        DELIVERED = 'delivered', 'Delivered',
+        CANCELED = 'canceled', 'Canceled'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='orders', on_delete=models.CASCADE, limit_choices_to={'role': 'buyer'})
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    total_amount = DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
