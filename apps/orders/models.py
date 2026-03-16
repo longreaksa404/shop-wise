bafb4c636cda59afceac97ea61f3b5e3ff6581a4
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 12:04:02 +0700

    implement cart item models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from apps.products.models import Product, Category
+
+class CartItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='cart_items', on_delete=models.CASCADE, limit_choices_to={'role': 'buyer'})
+    product = models. ForeignKey(Product, related_name='cart_items', on_delete=models.SET_NULL)
+    quantity = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
