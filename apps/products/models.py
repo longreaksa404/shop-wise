1765b047a143559c72e64f307dcb6f930d8e3f53
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 11:39:37 +0700

    implement product class

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from djoser.conf import settings
 
 
 class Category(models.Model):
@@ -10,11 +11,35 @@ class Category(models.Model):
 
     class Meta:
         db_table = 'categories'
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
         ordering = ('name',)
 
     def __str__(self):
         if self.parent_category:
             return f'{self.parent_category.name} - {self.name}'
         return self.name
+
+class Product(models.MOdel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True , related_name='products')
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products', limit_choices_to={'role': 'seller'})
+    image = models.ImageField(upload_to='products/', null=True, blank=True) # save to /media/products/
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f"{self.name} (${self.price})"
+
+    @property
+    def is_in_stock(self):
+        return self.stock_quantity > 0
