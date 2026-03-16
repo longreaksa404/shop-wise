56a08758c0374324a18d5b0ef3bcd891adf9a1d4
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-16 11:22:49 +0700

    implement category class

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    #allow add sub category
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ('name',)
+
+    def __str__(self):
+        if self.parent_category:
+            return f'{self.parent_category.name} - {self.name}'
+        return self.name
