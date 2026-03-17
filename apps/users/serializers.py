d03ac02b61cdce8143f4e06e1af52fb8465f2c2f
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-17 14:33:38 +0700

    implement validate func for prevent duplicate email

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -12,3 +12,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'password2', 'role', 'phone_number', 'address')
         extra_kwargs = {'role': {'required': False}}
 
+    def validate(self, value):
+        # if email already exists
+        if User.objects.first(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
