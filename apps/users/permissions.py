538f506e9ca80f9c96f0e00333f19023499bb6d9
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-23 11:05:45 +0700

    implement admin or seller and owner or admin

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -24,3 +24,18 @@ class IsBuyer(BasePermission):
             and request.user.is_buyer
         )
 
+class IsAdminOrSeller(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_admin or request.user.is_seller)
+        )
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj): # obj is the actual database record
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_admin or obj == request.user)
+        )
