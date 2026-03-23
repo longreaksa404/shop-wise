1241b7e97cb8987700f8b5d98a58b1456d9a9954
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-23 13:16:55 +0700

    implement login view

--- apps/users/views.py
+++ apps/users/views.py
@@ -51,3 +51,25 @@ class LoginView(APIView):
                 }
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            if not refresh_token:
+                return Response({
+                    'error': 'refresh token is required'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({
+                'message': 'success',
+            }, status=status.HTTP_200_OK)
+        except Exception:
+            return Response({
+                'error': 'invalid refresh token'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
