89806acca042795082b89d2db450222b0274e6c9
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-03-27 09:42:23 +0700

    implement url pattern

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path
-from rest_framework
+from rest_framework_simplejwt.views import TokenRefreshView
+from apps.users.views import RegisterView, LoginView, LogoutView, ProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+]
