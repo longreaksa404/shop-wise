02d85e090c78b56a27e1d02c659a893e80abf8d2
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-04-01 09:29:16 +0700

    create func to display parent category name

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -3,17 +3,19 @@ from apps.products.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    category_name = serializers.serializerMethodField(Category)
+    parent_category_name = serializers.serializerMethodField()
     subcategories = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ('id', 'name', 'parent_category', 'subcategories', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'parent_category', 'parent_category_name', 'subcategories', 'created_at', 'updated_at')
         read_only_fields = ('id', 'created_at', 'updated_at')
+
+    def get_parent_category_name(self, obj):
+        if obj.parent_category:
+            return obj.parent_category.name
+        return None
 
     def get_subcategories(self, obj):
         # return list of subcategory names
         return [sub.name for sub in obj.subcategories.all()]
-    
-    def __str__(self):
-        return self.category_name
