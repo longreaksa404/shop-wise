06c8c15163d220ff9e51afa08f63e383219c0c8e
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-04-03 09:33:59 +0700

    move UserManager class to Manager file

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,30 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
-
-class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Require email')
-        if not username:
-            raise ValueError('Require username')
-        email = self.normalize_email(email)  # convert to lowercases email format and prevent duplicate
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)  # hash password
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', User.Role.ADMIN)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Is staff must true')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Is superuser must true')
-
-        return self.create_user(email, username, password, **extra_fields)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from apps.users.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
