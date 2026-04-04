24706a4b7476b60635f59176e571021daeb18e9c
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-04-04 08:46:34 +0700

    add scope on throttle

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,5 +7,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     **REST_FRAMEWORK,
     'DEFAULT_THROTTLE_CLASSES': [],
-    'DEFAULT_THROTTLE_RATES': {},
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10000/day',
+        'user': '10000/day',
+        'login': '10000/minute',
+    },
 }
